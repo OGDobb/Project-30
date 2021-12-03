68151fb36ef97884efa59813a040d9146dec72fe
OGDobb <73088984+OGDobb@users.noreply.github.com>
2021-12-02 17:20:09 -0800

    Add files via upload

--- Block.js
+++ Block.js
@@ -4,17 +4,16 @@ class Block{
             isStatic:false,
             restitution:0,
             friction:1,
-            density:0.2
+            density:1.2
         }
         this.Visibility=255;
-        super(x,y,50,50);
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
         this.image = loadImage("grass_block.png");
         World.add(world, this.body);
       }
-      display(){
+      display() {
         var angle = this.body.angle;
         //fill("green");
         push();
@@ -22,22 +21,21 @@ class Block{
         rotate(angle);
         imageMode(CENTER);
         image(this.image, 0, 0, this.width, this.height);
-        //console.log(this.body.speed);
+        console.log(this.body.speed);
         
 
 
         if (this.body.speed<5) {
-          // imageMode(CENTER);
-          // image(this.image, 0, 0, this.width, this.height);
-          super.display();
+          imageMode(CENTER);
+          image(this.image, 0, 0, this.width, this.height);
         }
 
          else{
            World.remove(world,this.body);
           push();
-          tint(255,this.Visibility);
           this.Visibility=this.Visibility-5;
-          image(this.image,this.body.position.x,this.body.position.y,50,50);
+          tint(255,this.Visibility);
+          image(this.image,this.body.position.x,this.body.position.y,30,40);
           pop();
         }
         pop();
